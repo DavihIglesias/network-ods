7b38f933cf5c9e66ab53953b477b94a129270147
rafaelmesneves <rafaelmesquitaneves@gmail.com>
2024-11-11 17:38:16 -0300

    Componentes criados, falta outros icons, e demais conteudos

---
+++ client/src/MainContain.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../styles/mainContent.css';
+
+const MainContent = () => (
+  <main>
+    <img
+      src="https://www.danny.com.br/public/files/O-que-sao-ODS-Exemplos-de-como-aplica-los-nas-empresas.jpg"
+      alt="imagem-ods"
+    />
+    <h2>Lorem, ipsum dolor.</h2>
+    <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum excepturi dolorem, culpa exercitationem voluptates, repudiandae distinctio aspernatur eos nobis eum ab non modi ipsam suscipit quidem, dignissimos sunt incidunt inventore totam molestias molestiae repellat quos officia? Tenetur laudantium expedita mollitia perspiciatis molestias sapiente, rerum fugiat delectus deserunt ex porro deleniti.
+    </p>
+
+    <p>
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sapiente magnam consequuntur unde nihil corrupti ullam repudiandae beatae culpa harum mollitia assumenda itaque commodi officia ducimus recusandae, facere, ea pariatur expedita.
+    </p>
+
+    <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Id saepe quo libero tenetur quisquam laudantium quos in. Delectus, dignissimos omnis. Asperiores unde laudantium vero quas laboriosam animi maxime architecto, accusamus officia, voluptate at repellendus facilis cupiditate corporis molestiae. Illo, sapiente!
+    </p>
+
+    <div className="fim-main">
+      <button><a href="#">Mais informações</a></button>
+      <button><a href="#">Questionário</a></button>
+    </div>
+  </main>
+);
+
+export default MainContent;
---
+++ client/src/Navbar1.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../styles/navbar1.css';
+
+const Navbar1 = () => (
+  <nav className="navbar1">
+    <img src="https://i.postimg.cc/Y9Q9x0wZ/pngwing-com.png" alt="icon" />
+    <h1>Lorem, ipsum.</h1>
+    <div className="navbar1-link">
+      <a href="#">Login</a>
+      <a href="#">Signup</a>
+    </div>
+  </nav>
+);
+
+export default Navbar1;
